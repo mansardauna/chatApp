071554d98002b2f718638afc6383f86b174be17b
mansardauna <olaomansur@gmail.com>
2023-07-04 10:31:18 -0400

    chatmsg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+
 import React, { useRef, useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
@@ -7,6 +8,9 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import logo from './images/logo.svg'
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCb3ReqzrciEOXn3_RcoPapt6i7Ukn0Hu8",
   authDomain: "chatapp-a9809.firebaseapp.com",
@@ -31,8 +35,10 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <div class="">
+      <header class=' bg-emerald-100 shadow-md '>
+        <div class='flex w-fit p-2 m-auto items-center'>
+          <img src={logo} alt="logo" class='w-10 h-10' />
+          <div class=' font-sans font-light text-3xl'>OpenText</div>
         </div>
         <SignOut />
       </header>
@@ -54,8 +60,10 @@ function SignIn() {
 
   return (
     <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p class='text-lg'>Do not violate the community guidelines or you will be banned for life!</p>
+      <div class='w-6/12 shadow-md m-auto h-72 mt-48 bg-gradient-to-b from-green-100 to-slate-500  relative'>
+        <button className="sign-in" onClick={signInWithGoogle} class='mt-10 p-2 sign'>Sign in with Google</button>
+        <p class='text-lg absolute bottom-2 ml-5 text-white text-xs'>Have fun and connect to the world</p>
+      </div>
     </>
   )
 
@@ -63,7 +71,7 @@ function SignIn() {
 
 function SignOut() {
   return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()} class='p-2 bg-red-300 rounded-lg text-white font-bold absolute right-2 text-xs top-3'>Sign Out</button>
   )
 }
 
@@ -103,16 +111,15 @@ function ChatRoom() {
 
     </main>
 
-    <form onSubmit={sendMessage}>
+    <form onSubmit={sendMessage} class=' fixed bottom-0 m-auto w-full sm:w-10/12 sm:left-20 p-3 bg-emerald-100'>
 
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Say something" class='p-2 w-72 rounded-xl shadow-lg typing' />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      <button type="submit" disabled={!formValue} class='w-fit bg-slate-100 p-1 px-3 uppercase text-xs ml-3 rounded-lg shadow-lg'>Send</button>
 
     </form>
   </>)
 }
-
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
@@ -120,9 +127,11 @@ function ChatMessage(props) {
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt='dp' />
-      <p>{text}</p>
+    <div class='w-6/12 relative m-3 sm:m-auto mt-5'>
+      <div className={`message ${messageClass}`} class='p-2 px-4 rounded-xl mt-2 w-fit h-fit bg-gray-200 flex items-center'>
+        <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt='dp' class='w-10 rounded-full' />
+        <span class='ml-3'>{text}</span>
+      </div>
     </div>
   </>)
 }
